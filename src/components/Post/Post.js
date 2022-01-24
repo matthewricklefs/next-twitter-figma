2939619c5f61c45a1ecf08da32e5705fd5fc391c
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-24 10:11:02 -0500

    installs react icons, adjusts Home.module styles to introduce nexting, restructures Post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,26 @@
+import { FaHeart, FaShareAlt } from "react-icons/fa";
+
 import styles from "./Post.module.scss";
 
 const Post = ({ content, date }) => {
   return (
     <>
       <p className={styles.postsContent}>{content}</p>
+      <ul className={styles.postsMeta}>
+        <li className={styles.postsMetaData}>
+          <FaHeart />
+        </li>
 
-      <p className={styles.postsDate}>{date}</p>
+        <li className={styles.postsMetaData}>34</li>
+
+        <li className={styles.postsMetaData}>
+          <FaShareAlt />
+        </li>
+
+        <li className={styles.postsMetaData}>Share</li>
+
+        <li className={styles.postsMetaData}>{date}</li>
+      </ul>
     </>
   );
 };
